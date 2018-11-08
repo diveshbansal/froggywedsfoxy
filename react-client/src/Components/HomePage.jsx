f42bcc063c695a49cdb216aa050c9a7c119c03ec
divesh bansal <divesh.bansal@hotmail.com>
2018-11-08 18:39:03 +0530

    First iteration

---
+++ react-client/src/Components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class HomePage extends Component {
+  render() {
+    return (
+      <div class="home-page">
+        <h1>Amrita weds Divesh</h1>
+        <p>Love knows no bounds.</p>
+      </div>
+    )
+  }
+}
+export default HomePage;
